d5bd8e613123a9e77a701530366537cc6a18ea4c
Natalie Lukaniuk <natalie.lukaniuk@gmail.com>
2021-11-17 22:44:42 +0200

    added angular material

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,15 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
 
 import { HeaderComponent } from './containers/header/header.component';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    MatButtonModule
   ],
-  declarations: [HeaderComponent]
+  declarations: [HeaderComponent],
+  exports: [HeaderComponent, MatButtonModule]
 })
 export class SharedModule { }
