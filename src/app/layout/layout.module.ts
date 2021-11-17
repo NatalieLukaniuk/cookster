a74056b8d3bba88868a1f952db4b8f6142e08fd6
Natalie Lukaniuk <natalie.lukaniuk@gmail.com>
2021-11-17 22:16:31 +0200

    basic architecture

---
+++ src/app/layout/layout.module.ts
@@ -0,0 +1,12 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+
+import { MainLayoutComponent } from './containers/main-layout/main-layout.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [MainLayoutComponent]
+})
+export class LayoutModule { }
---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,12 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+
+import { HeaderComponent } from './containers/header/header.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [HeaderComponent]
+})
+export class SharedModule { }
