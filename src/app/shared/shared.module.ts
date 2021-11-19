b86b099da508e1e700bd8643aa32edfea140bb6f
Natalie Lukaniuk <natalie.lukaniuk@gmail.com>
2021-11-19 21:43:42 +0200

    navigation

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,15 +1,19 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
 
+import { NavigationComponent } from './components/navigation/navigation.component';
+import { SearchComponent } from './components/search/search.component';
 import { HeaderComponent } from './containers/header/header.component';
 
 @NgModule({
   imports: [
     CommonModule,
-    MatButtonModule
+    MatButtonModule,
+    MatSidenavModule
   ],
-  declarations: [HeaderComponent],
-  exports: [HeaderComponent, MatButtonModule]
+  declarations: [HeaderComponent, NavigationComponent, SearchComponent],
+  exports: [HeaderComponent, MatButtonModule, NavigationComponent, SearchComponent, MatSidenavModule]
 })
 export class SharedModule { }
