40570e024330e0f257c940214ab12dc9b7eefa79
Natalie Lukaniuk <natalie.lukaniuk@gmail.com>
2021-12-10 18:00:19 +0200

    converter service

--- src/app/recipies/services/amount-converter.service.spec.ts
+++ src/app/recipies/services/amount-converter.service.spec.ts
@@ -23,6 +23,13 @@ describe('Service: AmountConverter', () => {
     expect(amountInKg).toEqual(0.1);
   })
 
+  fit('should convert kg to gr', () => {
+    const converter = new AmountConverterService();
+    const amount = 10;
+    const amountInGr = converter.kgToGR(amount);
+    expect(amountInGr).toEqual(10000);
+  })
+
   fit('should convert gr to bunches', () => {
     const converter = new AmountConverterService();
     const amountInGr = 80;
@@ -30,11 +37,25 @@ describe('Service: AmountConverter', () => {
     expect(converted).toEqual(2);
   })
 
+  fit('should convert bunches to gr', () => {
+    const converter = new AmountConverterService();
+    const amount = 2;
+    const converted = converter.bunchToGr(amount);
+    expect(converted).toEqual(80);
+  })
+
   fit('should convert gr to pinches', () => {
     const converter = new AmountConverterService();
     const amountInGr = 6;
     const converted = converter.grToPinch(amountInGr);
     expect(converted).toEqual(2);
+  })
+
+  fit('should convert pinches to gr', () => {
+    const converter = new AmountConverterService();
+    const amount = 2;
+    const converted = converter.pinchToGr(amount);
+    expect(converted).toEqual(6);
   })
 
   fit('should convert gr to litres', () => {
@@ -45,11 +66,26 @@ describe('Service: AmountConverter', () => {
     expect(converted).toEqual(0.3);
   })
 
+  fit('should convert litres to gr', () => {
+    const converter = new AmountConverterService();
+    const amount = 0.3;
+    const density = 1000;
+    const converted = converter.literToGr(amount, density);
+    expect(converted).toEqual(300);
+  })
+
   fit('should convert litres to milliliters', () => {
     const converter = new AmountConverterService();
     const amountInL = 3;
     const converted = converter.literToMl(amountInL);
     expect(converted).toEqual(3000);
+  })
+
+  fit('should convert milliliters to litres', () => {
+    const converter = new AmountConverterService();
+    const amount = 3000;
+    const converted = converter.mlToL(amount);
+    expect(converted).toEqual(3);
   })
 
   fit('should convert grams to table spoons', () => {
@@ -60,12 +96,28 @@ describe('Service: AmountConverter', () => {
     expect(converted).toEqual(3);
   })
 
+  fit('should convert table spoons to grams', () => {
+    const converter = new AmountConverterService();
+    const amountInGr = 3;    
+    const density = 1000;
+    const converted = converter.tableSpoonsToGr(amountInGr, density);
+    expect(converted).toEqual(54);
+  })
+
   fit('should convert grams to tea spoons', () => {
     const converter = new AmountConverterService();
     const amountInGr = 15;    
     const density = 1000;
     const converted = converter.grToTeaSpoons(amountInGr, density);
     expect(converted).toEqual(3);
+  })
+
+  fit('should convert tea spoons to grams', () => {
+    const converter = new AmountConverterService();
+    const amountInGr = 3;    
+    const density = 1000;
+    const converted = converter.teaSpoonsToGr(amountInGr, density);
+    expect(converted).toEqual(15);
   })
 
   fit('should convert grams to dessert spoons', () => {
@@ -76,6 +128,14 @@ describe('Service: AmountConverter', () => {
     expect(converted).toEqual(15);
   })
 
+  fit('should convert dessert spoons to grams', () => {
+    const converter = new AmountConverterService();
+    const amountInGr = 15;    
+    const density = 1000;
+    const converted = converter.dessertSpoonsToGr(amountInGr, density);
+    expect(converted).toEqual(150);
+  })
+
   fit('should convert grams to coffee spoons', () => {
     const converter = new AmountConverterService();
     const amountInGr = 50;    
@@ -84,12 +144,28 @@ describe('Service: AmountConverter', () => {
     expect(converted).toEqual(20);
   })
 
+  fit('should convert coffee spoons to grams', () => {
+    const converter = new AmountConverterService();
+    const amountInGr = 20;    
+    const density = 1000;
+    const converted = converter.coffeeSpoonsToGr(amountInGr, density);
+    expect(converted).toEqual(50);
+  })
+
   fit('should convert grams to glasses', () => {
     const converter = new AmountConverterService();
     const amountInGr = 200;    
     const density = 1000;
     const converted = converter.grToGlass(amountInGr, density);
     expect(converted).toEqual(1);
+  })
+
+  fit('should convert glasses to grams', () => {
+    const converter = new AmountConverterService();
+    const amountInGr = 1;    
+    const density = 1000;
+    const converted = converter.glassToGr(amountInGr, density);
+    expect(converted).toEqual(200);
   })
   
 });
--- src/app/recipies/services/amount-converter.service.ts
+++ src/app/recipies/services/amount-converter.service.ts
@@ -8,20 +8,40 @@ export class AmountConverterService {
     return amount / 1000;
   }
 
+  kgToGR(amount: number) {
+    return amount * 1000;
+  }
+
   grToBunch(amount:number){
     return amount / 40;
+  }
+
+  bunchToGr(amount:number){
+    return amount * 40;
   }
 
   grToPinch(amount:number){
     return amount / 3;
   }
 
+  pinchToGr(amount:number){
+    return amount * 3;
+  }
+
   grToLiter(amount: number, density: number){ // density is in kg/m3
     return amount / density;
   }
 
+  literToGr(amount: number, density: number){ // density is in kg/m3
+    return amount * density;
+  }
+
   literToMl(amount: number){
     return amount * 1000;
+  }
+
+  mlToL(amount: number){
+    return amount / 1000;
   }
 
   grToTableSpoons(amount: number, density: number){ // density is in kg/m3
@@ -30,10 +50,24 @@ export class AmountConverterService {
     return ml / 18;
   }
 
+  tableSpoonsToGr(amount: number, density: number){ // density is in kg/m3
+    const ml = amount * 18;
+    const l = this.mlToL(ml);
+    const gr = this.literToGr(l, density);
+    return gr;
+  }
+
   grToTeaSpoons(amount: number, density: number){ // density is in kg/m3
     const l = this.grToLiter(amount, density);
     const ml = this.literToMl(l);
     return ml / 5;
+  }
+
+  teaSpoonsToGr(amount: number, density: number){ // density is in kg/m3
+    const ml = amount * 5;
+    const l = this.mlToL(ml);
+    const gr = this.literToGr(l, density);
+    return gr;
   }
 
   grToDessertSpoons(amount: number, density: number){ // density is in kg/m3
@@ -42,16 +76,37 @@ export class AmountConverterService {
     return ml / 10;
   }
 
+  dessertSpoonsToGr(amount: number, density: number){ // density is in kg/m3
+    const ml = amount * 10;
+    const l = this.mlToL(ml);
+    const gr = this.literToGr(l, density);
+    return gr;
+  }
+
   grToCoffeeSpoons(amount: number, density: number){ // density is in kg/m3
     const l = this.grToLiter(amount, density);
     const ml = this.literToMl(l);
     return ml / 2.5;
   }
 
+  coffeeSpoonsToGr(amount: number, density: number){ // density is in kg/m3
+    const ml = amount * 2.5;
+    const l = this.mlToL(ml);
+    const gr = this.literToGr(l, density);
+    return gr;
+  }
+
   grToGlass(amount: number, density: number){ // density is in kg/m3, glass can fit 200ml
     const l = this.grToLiter(amount, density);
     const ml = this.literToMl(l);
     return ml / 200;
+  }
+
+  glassToGr(amount: number, density: number){ // density is in kg/m3, glass can fit 200ml
+    const ml = amount * 200;
+    const l = this.mlToL(ml);
+    const gr = this.literToGr(l, density);
+    return gr;
   }
 
   
