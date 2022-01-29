6e6ccb154cb2e6d28a9e7631df4264dee66addd7
Natalie Lukaniuk <natalie.lukaniuk@gmail.com>
2022-01-29 21:16:16 +0200

    mobileL fixed tabs and header

--- src/app/shared/containers/header/header.component.ts
+++ src/app/shared/containers/header/header.component.ts
@@ -1,13 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import { Router } from '@angular/router';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
+
+import { LayoutService } from '../../services/layout.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent {
-  constructor(private router: Router) {}
+export class HeaderComponent implements OnDestroy {
+  isMobile: boolean = false;
+  destroy$ = new Subject();
+  constructor(private router: Router, private layoutService: LayoutService) {
+    this.layoutService.isMobile$
+      .pipe(takeUntil(this.destroy$))
+      .subscribe((bool) => this.isMobile = bool);
+  }
+  ngOnDestroy(): void {
+    this.destroy$.next();
+  }
 
   goHome() {
     this.router.navigate(['']);
