60bbf6013b7d7a705735be48ada626559420a17f
nafadieieva <nafadieieva@deloitte.ua>
2022-06-04 11:20:08 +0300

    ingredients filters ui

--- src/app/filters/components/filters/filters.component.ts
+++ src/app/filters/components/filters/filters.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Subject } from 'rxjs';
+import { Product } from 'src/app/recipies/models/products.interface';
+import { RecipiesService } from 'src/app/recipies/services/recipies.service';
 
 @Component({
   selector: 'app-filters',
   templateUrl: './filters.component.html',
   styleUrls: ['./filters.component.scss']
 })
-export class FiltersComponent implements OnInit {
+export class FiltersComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  products: Product[] = [];
 
-  ngOnInit(): void {
+  destroyed$ = new Subject();
+
+  constructor(private recipiesService: RecipiesService, private store: Store) { }
+
+  ngOnInit(): void {    
+    this.recipiesService.products$.subscribe((products: Product[]) => {
+      this.products = products;
+    })
+  }
+
+  ngOnDestroy(): void {
+    this.destroyed$.complete()
   }
 
 }
