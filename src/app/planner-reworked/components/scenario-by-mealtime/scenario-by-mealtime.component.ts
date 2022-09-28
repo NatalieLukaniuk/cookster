bf0b605b7f388783f7d39499f472c09e9dcb3548
Natalie Lukaniuk <natalie.lukaniuk@gmail.com>
2022-09-28 11:16:37 +0300

    recipies tab in scenario

--- src/app/planner-reworked/components/scenario-by-mealtime/scenario-by-mealtime.component.ts
+++ src/app/planner-reworked/components/scenario-by-mealtime/scenario-by-mealtime.component.ts
@@ -1,15 +1,62 @@
-import { Component, OnInit } from '@angular/core';
+import { RecipyForCalendar } from './../../../recipies/models/recipy.interface';
+import { MealTime } from 'src/app/calendar/components/celandar-meal/celandar-meal.component';
+import { map, takeUntil } from 'rxjs/operators';
+import { Observable, Subject, combineLatest } from 'rxjs';
+import { getCurrentRoute } from './../../../store/selectors/ui.selectors';
+import { select, Store } from '@ngrx/store';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { IAppState } from 'src/app/store/reducers';
+import { getCalendar } from 'src/app/store/selectors/calendar.selectors';
+import { Day } from 'src/app/calendar/components/calendar/calendar.component';
+import { Recipy } from 'src/app/recipies/models/recipy.interface';
 
 @Component({
   selector: 'app-scenario-by-mealtime',
   templateUrl: './scenario-by-mealtime.component.html',
-  styleUrls: ['./scenario-by-mealtime.component.scss']
+  styleUrls: ['./scenario-by-mealtime.component.scss'],
 })
-export class ScenarioByMealtimeComponent implements OnInit {
+export class ScenarioByMealtimeComponent implements OnInit, OnDestroy {
+  destroy$ = new Subject();
+  mealtime: MealTime | undefined;
+  date: string | undefined;
+  calendar$: Observable<Day[] | null>;
+  currentRoute$: Observable<string>;
 
-  constructor() { }
-
-  ngOnInit() {
+  recipies: RecipyForCalendar[] | undefined;
+  constructor(private store: Store<IAppState>) {
+    this.calendar$ = this.store.pipe(select(getCalendar));
+    this.currentRoute$ = this.store.pipe(select(getCurrentRoute));
+  }
+  ngOnDestroy(): void {
+    this.destroy$.next();
   }
 
+  ngOnInit() {
+    combineLatest([this.currentRoute$, this.calendar$])
+      .pipe(
+        takeUntil(this.destroy$),
+        map((res) => ({ route: res[0], calendar: res[1] }))
+      )
+      .subscribe((res) => {
+        if (res.route && res.calendar) {
+          this.mealtime = res.route.split('_')[1] as MealTime;
+          this.date = res.route.split('_')[0];
+          let foundDay = res.calendar.find(
+            (day) => day.details.day == this.date
+          );
+          if (foundDay) {
+            switch (this.mealtime) {
+              case MealTime.Breakfast:
+                this.recipies = foundDay.details.breakfastRecipies;
+                break;
+              case MealTime.Lunch:
+                this.recipies = foundDay.details.lunchRecipies;
+                break;
+              case MealTime.Dinner:
+                this.recipies = foundDay.details.dinnerRecipies;
+            }
+          }
+        }
+      });
+  }
 }
